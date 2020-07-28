51f282abfa1648b6922ef22b00ac22f454b159c4
PRG-YouTube <prgu2be@gmail.com>
2020-07-28 12:55:20 +0200

    Content update

--- js/js.js
+++ js/js.js
@@ -1,31 +1,27 @@
 ( function() {
+	var youtube = document.querySelectorAll( ".youtube" );
 
-    var youtube = document.querySelectorAll( ".youtube" );
-    
-    for (var i = 0; i < youtube.length; i++) {
-        
-        var source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed +"/sddefault.jpg";
-        //var source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed +"/maxresdefault.jpg";
-		
-        var image = new Image();
-                image.src = source;
-	    	image.alt = "Video with title: " + youtube[i].dataset.title;
-	    	image.title = "Click to play video! " + youtube[i].dataset.title;
-                image.addEventListener( "load", function() {
-                    youtube[ i ].appendChild( image );
-                }( i ) );
-        
-                youtube[i].addEventListener( "click", function() {
+	for (var i = 0; i < youtube.length; i++) {
+		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+		//var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/maxresdefault.jpg";
 
-                    var iframe = document.createElement( "iframe" );
+		var image = new Image();
+		image.src = source;
+		image.alt = "Video with title: "+ youtube[i].dataset.title;
+		image.title = "Click to play video! "+ youtube[i].dataset.title;
+		image.addEventListener( "load", function() {
+			youtube[ i ].appendChild( image );
+		} ( i ) );
 
-                            iframe.setAttribute( "frameborder", "0" );
-                            iframe.setAttribute( "allowfullscreen", "" );
-                            iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+		youtube[i].addEventListener( "click", function() {
+			var iframe = document.createElement( "iframe" );
+			iframe.setAttribute( "frameborder", "0" );
+			iframe.setAttribute( "allowfullscreen", "" );
+			iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
 
-                            this.innerHTML = "";
-                            this.appendChild( iframe );
-                } );    
-    };
-    
-} )();
+			this.innerHTML = "";
+			this.appendChild( iframe );
+		} );	
+	};
+} ) ();
+
